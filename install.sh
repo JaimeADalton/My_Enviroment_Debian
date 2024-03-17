62eb7b3774ce9ebdaf34df2f17b001c1b004ee03
JaimeADalton <jaimeadalton@gmail.com>
2024-03-17 22:25:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #/bin/bash
 
 # INstalar paquetes basicos
-
+apt update
 apt install sudo curl wget vim nitrogen firefox-esr awesome lightdm nautilus hydra nmap nfs-common nfstrace nmon alacritty
 
 echo "modificar el archivo /etc/profile y .bashrc de jaimedalton y de root"
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+mkdir -p /home/user/{Downloads,Documents,Pictures,Music,Videos,Desktop}
 
 mkdir -p /home/jaimedalton/.config/awesome
 mkdir -p /root/.config/awesome
 
-cp /etc/xdg/awesome/rc.lua /home/jaimedalton/.config/awesome
-cp /etc/xdg/awesome/rc.lua /root/.config/awesome
-
+wget -O /home/jaimedalton/.config/awesome/rc.lua https://raw.githubusercontent.com/JaimeADalton/My_Enviroment_Debian/main/rc.lua
