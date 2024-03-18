144287bc0a779b3ebb71becdb2f6f3172d170635
JaimeADalton <jaimeadalton@gmail.com>
2024-03-18 16:44:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # INstalar paquetes basicos
 apt update
-apt install sudo curl wget vim nitrogen firefox-esr awesome lightdm nautilus hydra nmap nfs-common nfstrace nmon alacritty
+apt install sudo curl wget vim nitrogen firefox-esr awesome lightdm nautilus hydra nmap nfs-common nfstrace nmon alacritty i3lock remmina
 
 echo "modificar el archivo /etc/profile y .bashrc de jaimedalton y de root"
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
@@ -12,3 +12,6 @@ mkdir -p /home/jaimedalton/.config/awesome
 mkdir -p /root/.config/awesome
 
 wget -O /home/jaimedalton/.config/awesome/rc.lua https://raw.githubusercontent.com/JaimeADalton/My_Enviroment_Debian/main/rc.lua
+
+#Configurar sudoers para usuario principal NOPASSWD
+#
